b8cf9d200cedb6f85212a90b531e0044c2ec6eaa
nil147 <68010556+nil147@users.noreply.github.com>
2021-02-21 11:51:36 +0200

    update

--- slack/src/App.js
+++ slack/src/App.js
@@ -1,13 +1,27 @@
 import React from 'react';
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="app">
-      <h1>hello</h1>
+      <Router>
+        <>
+          <Switch>
+            <Route path="/">
+              <Header />
+            </Route>
+          </Switch>
+        </>
+      </Router>
     </div>
   );
 }
---
+++ slack/src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from "styled-components";
+
+function Header() {
+    return (
+        <headerContainer>
+
+        </headerContainer>
+    )
+}
+
+export default Header
+
+
+const headerContainer = styled.div`
+>h1 {
+    color: "red";
+}
+  color: "red";
+`;
