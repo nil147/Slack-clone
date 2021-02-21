565232d4d389ff3c11b3cf4b9fad6f789e908638
nil147 <68010556+nil147@users.noreply.github.com>
2021-02-21 11:08:03 +0200

    first upload

---
+++ slack/src/firbase.js
@@ -0,0 +1,11 @@
+import firebase from ''
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCTPOicWgg0AyP3Sd9I-xXFW7ssi-PTfd0",
+    authDomain: "slack-clone-704eb.firebaseapp.com",
+    projectId: "slack-clone-704eb",
+    storageBucket: "slack-clone-704eb.appspot.com",
+    messagingSenderId: "892695017838",
+    appId: "1:892695017838:web:d150bb63da64d21828697f"
+};
